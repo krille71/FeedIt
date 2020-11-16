4d0d4accefade884da87e6fc49f49d3db42ffd09
davidmagnussson <davidmagnusson97@gmail.com>
2020-11-16 23:03:55 +0100

    Added scripts to track ingredient collision, it currently only log the tag while waiting on decision on how to spawn the ingredients randomnely on the chunks. Also created 3 prefabs containing this script from the placeholder ingredients

---
+++ FeedIt/Assets/Scripts/CollectIngredients.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectIngredients : MonoBehaviour
+{
+    string ingredient;
+
+    private void OnTriggerEnter2D(Collider2D other){
+        if(other.gameObject.CompareTag("Player")){
+            ingredient = gameObject.tag;
+            CookingHandling.instance.HandleIngredientCollecting(ingredient);
+        }
+    }
+}
---
+++ FeedIt/Assets/Scripts/CookingHandling.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CookingHandling : MonoBehaviour
+{
+    public static CookingHandling instance;
+
+    void Start()
+    {
+        if(instance == null){
+            instance = this;
+        }
+    }
+
+    public void HandleIngredientCollecting(string ingredient){
+        Debug.Log("Collected: " + ingredient);
+    }
+}
