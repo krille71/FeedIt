6c01f3e9bed86a5ebdc43e4bc29c5588f98fb6d4
davidmagnussson <davidmagnusson97@gmail.com>
2020-11-17 18:58:04 +0100

    Updated the graphics with our own. Made prefabs of all the objects and also started with the cookingHUD. It only includes placeholders which do not update on collision

--- FeedIt/Assets/Scripts/CookingHandling.cs
+++ FeedIt/Assets/Scripts/CookingHandling.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class CookingHandling : MonoBehaviour
 {
@@ -14,6 +15,10 @@ public class CookingHandling : MonoBehaviour
     }
 
     public void HandleIngredientCollecting(GameObject ingredient){
+        //GameObject activeIngredientSlot = GameObject.Find("Ingredient_slot_1").gameObject;
+        //Image activeIngredient = ingredient.GetComponent<Image>();
         Debug.Log("Collected: " + ingredient);
+        //Debug.Log(activeIngredient.image);
+
     }
 }
