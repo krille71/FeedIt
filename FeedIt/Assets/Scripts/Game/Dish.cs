87aeaad4f47c4a91f4744b5bd3a8ee435e47e12e
kalmroth <kalmroth@kth.se>
2020-12-02 14:52:25 +0100

    Added BowlOfGoods and BowlOfGoodsTranquilizer

---
+++ FeedIt/Assets/Scripts/Game/Dish.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dish : MonoBehaviour
+{
+    [SerializeField] private float BEAST_EATING_MOVEMENT = 0.5f;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Beast"))
+        {
+            other.gameObject.GetComponent<Beast>().Move(BEAST_EATING_MOVEMENT);
+            Destroy(gameObject);
+        }
+    }
+}
