f9189fe661d6d6fef85b4a1b6f7ac6800cfc2691
kalmroth <kalmroth@kth.se>
2020-12-04 19:54:37 +0100

    Score display that counts up each second

---
+++ FeedIt/Assets/Scripts/Game/ScoreCounter.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ScoreCounter : MonoBehaviour
+{
+
+    private float timer = 0;
+    private int score = 0;
+    private TextMeshProUGUI scoreDisplay;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        scoreDisplay = GameObject.FindGameObjectWithTag("ScoreDisplay").GetComponent<TextMeshProUGUI>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        score = (int)timer;
+        scoreDisplay.text = score.ToString();
+    }
+}
