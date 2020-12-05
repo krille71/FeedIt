72260518ab1835b2ea92b9d2dd0526f173fc9efd
kalmroth <kalmroth@kth.se>
2020-12-05 13:35:27 +0100

    Score text grayed out when game is paused

--- FeedIt/Assets/Scripts/Game/ScoreCounter.cs
+++ FeedIt/Assets/Scripts/Game/ScoreCounter.cs
@@ -22,6 +22,16 @@ public class ScoreCounter : MonoBehaviour
         timer += Time.deltaTime;
         score = (int)timer;
         scoreDisplay.text = score.ToString();
+
+        // Grey out text if paused
+        if (InGameMenu.GameIsPaused)
+        {
+            scoreDisplay.color = new Color32(255, 255, 255, 155);
+        }
+        else
+        {
+            scoreDisplay.color = new Color32(255, 255, 255, 255);
+        }
     }
 
     public int GetScore()
