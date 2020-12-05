ea50f1bd70540c2d4f7084192ca0283012b77876
kalmroth <kalmroth@kth.se>
2020-12-05 14:04:08 +0100

    Increased fall gravity and made jump more responsive when mounted

--- FeedIt/Assets/Scripts/Game/PlayerRacoon.cs
+++ FeedIt/Assets/Scripts/Game/PlayerRacoon.cs
@@ -7,7 +7,7 @@ public class PlayerRacoon : Player
     protected Animator raccoonAnim;
 
     [SerializeField] private PlayerOstrich ostrich;
-    [SerializeField] private float DETACH_TIME = 0.3f;
+    [SerializeField] private float DETACH_TIME = 0.0f;
     [SerializeField] private float RETACH_TIME = 0.3f;
     [SerializeField] private Vector3 localPosition = new Vector3(-0.31f, 0.44f, -0.1f);
 
@@ -17,40 +17,53 @@ public class PlayerRacoon : Player
     bool overOstrich = false;
     bool prevOverOstrich = false;
 
+    bool pressedJumpKey = false;
+    bool pressedDownKey = false;
+
     private void Awake(){
         raccoonAnim = GetComponent<Animator>();
 
     }
 
-
     protected override void KeyInput()
     {
-
         // Pressed keys
         if (Input.GetKeyDown(KeyCode.W))
+        {
             jumpBuffer = JUMP_BUFFER_TIME;
+            pressedJumpKey = true;
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+            pressedDownKey = true;
 
         // Held keys
         if (Input.GetKey(KeyCode.W))
             holdingJumpKey = true;
         if (Input.GetKey(KeyCode.S))
             holdingDownKey = true;
-            }
+    }
+
+    protected override void Update()
+    {
+        pressedJumpKey = false;
+        pressedDownKey = false;
+        base.Update();
+    }
 
 
     protected override void HandleInput()
     {
         // Detach
-        if (transform.parent != null && detachTimer < 0)
+        if (transform.parent != null && detachTimer < 0 && !CookingHandling.isCooking)
         {
-            if (holdingJumpKey || holdingDownKey)
+            if (pressedJumpKey || pressedDownKey)
             {
                 transform.parent = null;
                 retachTimer = RETACH_TIME;
                 rigidbody.isKinematic = false;
 
                 // Fix jumping velocity for the racoon when the ostrich is jumping
-                //if(ostrich.transform.GetComponent<Rigidbody2D>().velocity.y < 0 && holdingDownKey)
+                // if(ostrich.transform.GetComponent<Rigidbody2D>().velocity.y < 0 && holdingDownKey)
                 rigidbody.velocity = Vector3.zero;
             }
         }
@@ -71,11 +84,13 @@ public class PlayerRacoon : Player
     protected override void FixedUpdate()
     {
         base.FixedUpdate();
+
         // racoons positin in relation to the ostrich
         prevOverOstrich = overOstrich;
         var localPos = ostrich.transform.InverseTransformPoint(transform.position);
         overOstrich = localPos.y > localPosition.y;
         UpdateRaccoonAnimation(rigidbody.velocity.y);
+
         // Decrease cooldowns
         retachTimer -= Time.deltaTime;
         detachTimer -= Time.deltaTime;
